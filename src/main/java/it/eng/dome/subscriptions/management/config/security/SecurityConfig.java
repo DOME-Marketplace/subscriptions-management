962a057c13fc2775ec26878cece823c5835bf269
Di Zenzo Carmine Federico <191864161+cdizenzo@users.noreply.github.com>
2025-12-11 16:47:58 +0100

    Enhance authentication and authorization: add token utilities, update security configuration, and implement role-based access control

--- src/main/java/it/eng/dome/subscriptions/management/config/security/SecurityConfig.java
+++ src/main/java/it/eng/dome/subscriptions/management/config/security/SecurityConfig.java
@@ -1,38 +1,52 @@
-//package it.eng.dome.subscriptions.management.config.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    JwtAuthenticationConverter jwtAuthenticationConverter() {
-//        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-//        converter.setJwtGrantedAuthoritiesConverter(new JwtRoleConverter());
-//        return converter;
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable()) // necessario per API REST
-//                .authorizeHttpRequests(auth -> auth
-//                        // PERMETTE accesso anonimo ai file statici
-//                        .requestMatchers("/", "/index.html", "/css/**", "/js/**", "/static/**").permitAll()
-//                        // Protegge solo le API
-//                        .requestMatchers("/management/**").authenticated()
-//                        .anyRequest().permitAll() // altre rotte anonime
-//                )
-//                .oauth2ResourceServer(oauth2 ->
-//                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
-//                );
-//
-//        return http.build();
-//    }
-//}
+package it.eng.dome.subscriptions.management.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new JwtRoleConverter());
+        return converter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .csrf(csrf -> csrf.disable()) // necessario per API REST
+
+                // same-origin cors not needed
+                .cors(cors -> cors.disable())
+
+                .authorizeHttpRequests(auth -> auth
+
+                        // all static resources are permitted
+                        .requestMatchers("/**", "/css/**", "/js/**", "/media/**").permitAll()
+
+                        // all APIs under /management/** require authentication
+                        .requestMatchers("/configuration", "/organizations/**", "/plans/**").authenticated()
+
+                        // all other requests are permitted
+                        .anyRequest().permitAll()
+                )
+                // enable resource server to use JWT tokens
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
+
+        return http.build();
+    }
+}
